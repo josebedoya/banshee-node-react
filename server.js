de13ca3e9a85a866bdf5083a65c90723fbc7c91d
Jose Bedoya <jossephb@gmail.com>
2019-10-07 22:28:22 -0500

    create database tables and their associations

--- server.js
+++ server.js
@@ -10,14 +10,40 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Models
+const User = require('./models/user');
+const Client = require('./models/client');
+const Visit = require('./models/visit');
+const Comment = require('./models/comment');
+const Agent = require('./models/agent');
+const Country = require('./models/country');
+const State = require('./models/state');
+const City = require('./models/city');
+
 // Define Routes
-app.use('/api/clients/', require('./routes/api/clients'));
-app.use('/api/agents/', require('./routes/api/agents'));
-app.use('/api/visits/', require('./routes/api/visits'));
-app.use('/api/countries/', require('./routes/api/countries'));
-app.use('/api/states/', require('./routes/api/states'));
-app.use('/api/cities/', require('./routes/api/cities'));
-app.use('/api/comments/', require('./routes/api/comments'));
+//app.use('/api/auth/', require('./routes/api/auth'));
+//app.use('/api/clients/', require('./routes/api/clients'));
+// app.use('/api/agents/', require('./routes/api/agents'));
+// app.use('/api/visits/', require('./routes/api/visits'));
+// app.use('/api/countries/', require('./routes/api/countries'));
+// app.use('/api/states/', require('./routes/api/states'));
+// app.use('/api/cities/', require('./routes/api/cities'));
+// app.use('/api/comments/', require('./routes/api/comments'));
+
+// Create db relationships
+Visit.belongsTo(Client, { constraints: false });
+Visit.belongsTo(Agent, { constraints: false });
+Comment.belongsTo(Visit, { constraints: false });
+State.belongsTo(Country, { constraints: false });
+City.belongsTo(Country, { constraints: false });
+City.belongsTo(State, { constraints: false });
+Client.belongsTo(Country, { constraints: false });
+Client.belongsTo(State, { constraints: false });
+Client.belongsTo(City, { constraints: false });
+//
+Visit.belongsTo(User, { constraints: false });
+Comment.belongsTo(User, { constraints: false });
+Client.belongsTo(User, { constraints: false });
 
 sequelize
   .sync()
